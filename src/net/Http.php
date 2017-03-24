0d37c892e1ecd1c015479320207d579cf0f976bd
chenyan <2545205707@qq.com>
2017-03-24 17:35:34 +0800

    ttttttttttt

--- src/net/Http.php
+++ src/net/Http.php
@@ -3,8 +3,31 @@ namespace danpai\net;
 
 class Http
 {
-	public function get()
+	public static function multiGet($urls = [])
 	{
-		echo time();
+		$mh = curl_multi_init();
+		$handles = [];
+		foreach ($urls as $key => $url) {
+			$handles[$key] = curl_init($url);
+			curl_setopt($handles[$key], CURLOPT_RETURNTRANSFER, true);
+			curl_multi_add_handle($mh, $handles[$key]);
+		}
+
+		$running = null;
+	  	do {
+	     	curl_multi_exec($mh, $running);
+	   	} while ($running);
+
+	   	$responses = [];
+	   	foreach ($handles as $key => $handle) {
+	   		$responses[] = curl_multi_remove_handle($mh, $handle);
+	   	}
+
+	   	return $responses;
+	}
+
+	public static function get()
+	{
+
 	}
 }
--- test.php
+++ test.php
@@ -1,6 +1,2 @@
 <?php
 require __DIR__.'/vendor/autoload.php';
-
-(new danpai\net\Http())->get();
-
-(new danpai\string\Text())->show();
